e1927854c73043f41770a3c8a986ebeb8055189f
Jonathan Melly <jonathan.melly@gmail.com>
2023-09-01 12:05:49 +0200

    feat(snail): added an alternative draft of solution...

---
+++ exos/snail/corrige-alternatif/Program.cs
@@ -0,0 +1,89 @@
+﻿using SnailJMY1;
+
+var snail1 = new Snail();
+snail1.Nickname = "steph";
+snail1.Y = 1;
+
+var snail2 = new Snail();
+snail2.Nickname = "bob";
+snail2.Y = 2;
+
+snail1.BeReady();
+snail2.BeReady();
+
+for (int i = 0; i < 20; i++)
+{
+    snail1.MoveRight();
+    snail2.MoveRight();
+
+    Thread.Sleep(250);
+}
+
+
+/*
+for (int i = 0; i < 10; i++)
+{
+    
+}
+Console.Write("   \n   ");
+var hp1 = 50;
+var hp2 = 50;
+var ySnail1 = 0;
+var ySnail2 = 1;
+
+while (true)
+{
+    if (Console.KeyAvailable)
+    {
+        var key = Console.ReadKey(true);
+        if (key.Key == ConsoleKey.RightArrow)
+        {
+            Console.CursorTop = ySnail1;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.CursorLeft -= 3;
+            Console.Write("_");
+            Console.Write("_o°");
+            hp1--;
+            if(hp1==0) 
+            {
+                Console.Clear();
+                Console.Write("__RIP__");
+                break;
+            }
+        }
+
+        if (key.Key == ConsoleKey.LeftArrow)
+        {
+            Console.CursorTop = ySnail2;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.CursorLeft -= 3;
+            Console.Write("_");
+            Console.Write("_o°");
+            hp2--;
+            if (hp2 == 0)
+            {
+                Console.Clear();
+                Console.Write("__RIP__");
+                break;
+            }
+        }
+
+        if (key.Key == ConsoleKey.RightArrow)
+        {
+            Console.CursorTop = ySnail1;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.CursorLeft -= 3;
+            Console.Write("_");
+            Console.Write("_o°");
+            hp1--;
+            if (hp1 == 0)
+            {
+                Console.Clear();
+                Console.Write("__RIP__");
+                break;
+            }
+        }
+    }
+    
+}
+*/
---
+++ exos/snail/corrige-alternatif/Snail.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnailJMY1
+{
+    internal class Snail
+    {
+
+        string nickname = "bob";
+
+        ConsoleColor color = ConsoleColor.Gray;
+        int hp=50;
+        int x=0;
+        int y;
+
+        string skin="_O°";
+
+        public string Nickname { get => nickname; set => nickname = value; }
+        public int Y { get => y; set => y = value; }
+
+        public void BeReady()
+        {
+            Console.CursorLeft = 0;
+            Console.CursorTop = y;
+            Console.Write(nickname+new string(' ',skin.Length));
+            x = Console.CursorLeft;
+        }
+
+        public void MoveRight()
+        {
+            //Position
+            Console.CursorTop = y;
+            Console.CursorLeft = x;
+
+            //Couleur
+            Console.ForegroundColor = color;
+
+            //Effacer l’ancienne position
+            Console.CursorLeft-=skin.Length;
+            Console.Write(" ");
+
+            //Maj hp
+            hp--;
+            if (hp == 0)
+            {
+                Console.Write("__RIP__");
+            }
+            else
+            {
+                Console.Write(skin);
+            }
+
+            x = x + 1;
+            
+        }
+    }
+}
