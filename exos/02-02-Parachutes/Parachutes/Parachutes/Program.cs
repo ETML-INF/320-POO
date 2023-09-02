eadd2dbd236115647b9afc03d536e6e0bc57777a
Xavier Carrel <px05iql@eduvaud.ch>
2023-09-02 10:07:35 +0200

    feat(para): initialize console window

---
+++ exos/02-02-Parachutes/Parachutes/Parachutes/Program.cs
@@ -0,0 +1,10 @@
+﻿using Parachutes;
+
+const int SCREEN_HEIGHT = 40;
+const int SCREEN_WIDTH = 150;
+
+Console.Clear();
+Console.CursorVisible = false;
+Console.WindowHeight = SCREEN_HEIGHT;
+Console.WindowWidth = SCREEN_WIDTH;
+Console.ReadKey();
