7e1761d37d6265238acae62b9584c85431bdbdd1
Xavier Carrel <Xavier.Carrel@eduvaud.ch>
2025-09-20 13:16:26 +0200

    feat(Drone): Ajouter l'Id à la factory

--- exos/Drones/Drones/Drones/Model/Box.cs
+++ exos/Drones/Drones/Drones/Model/Box.cs
@@ -4,9 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Drones.Model
+namespace Drones
 {
-    internal class Box
+    public class Box
     {
+        private static int _lastId = 1;
+
+        private int _id;
+        private int _weight;
+        private string _color;
+
+        public Box()
+        {
+            _id = _lastId++;
+            _weight = GlobalHelpers.alea.Next(5, 11);
+            switch (GlobalHelpers.alea.Next(1, 6))
+            {
+                case 1: _color = "rouge"; break;
+                case 2: _color = "jaune"; break;
+                case 3: _color = "bleu"; break;
+                case 4: _color = "brun"; break;
+                case 5: _color = "orange"; break;
+                default: _color = "???"; break;
+            }
+        }
+        public Box(int weight, string color)
+        {
+            _id = _lastId++;
+            _weight = weight;
+            _color = color;
+        }
     }
 }
--- exos/Drones/Drones/Drones/Model/Factory.cs
+++ exos/Drones/Drones/Drones/Model/Factory.cs
@@ -8,10 +8,15 @@ namespace Drones
 {
     public class Factory : Building
     {
+        private static int _lastId = 1;
+        
+        private static int _id;
+
         private double _powerConsumption;
         public double PowerConsumption { get => _powerConsumption;}
         public Factory(int x, int y, double powerConsumption) : base(x, y, Color.Blue)
         {
+            _id = _lastId++;
             _powerConsumption = powerConsumption;
             Console.WriteLine($"New Fatory consuming: {PowerConsumption} kWh");
         }
