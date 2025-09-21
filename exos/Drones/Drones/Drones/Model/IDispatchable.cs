92878230c42719a195e000d1acac57878139bd90
Xavier Carrel <Xavier.Carrel@eduvaud.ch>
2025-09-21 08:50:30 +0200

    feat(drones): Place many factories and stores at pseudo-random places

--- exos/Drones/Drones/Drones/Model/IDispatchable.cs
+++ exos/Drones/Drones/Drones/Model/IDispatchable.cs
@@ -8,7 +8,11 @@ namespace Drones
 {
     public interface IDispatchable
     {
+        // Dépose d'une boîte de smarties 
         public void DropBox(Box box);
+
+        // Prise d'une boîte de smarties
+        // Retourne `null` s'il n'y a pas de boîte en stock
         public Box? Pickup();
 
     }
--- exos/Drones/Drones/Drones/Program.cs
+++ exos/Drones/Drones/Drones/Program.cs
@@ -17,9 +17,21 @@ namespace Drones
             fleet.Add(new Drone(AirSpace.WIDTH / 2, AirSpace.HEIGHT / 2, "Joe"));
 
             List<Building> buildings= new List<Building>();
-            buildings.Add(new Factory(20, 20, 2500.0));
-            buildings.Add(new Factory(120, 20,  4000));
-            buildings.Add(new Store(20, 220, new List<string>() { "Lu : 8h-18h", "Ma : 8h-18h", "Me : 8h-18h", "Je : 8h-18h", "Ve : 8h-18h", "Sa-Di : Fermé" }));
+            for (int f = 0; f < 4; f++)
+            {
+                buildings.Add(new Factory(
+                    AirSpace.WIDTH / 8 + f * AirSpace.WIDTH / 4 + GlobalHelpers.alea.Next(0,100)-50, 
+                    AirSpace.HEIGHT / 5 + GlobalHelpers.alea.Next(0, 100) - 50, 
+                    GlobalHelpers.alea.Next(2000, 10000)));
+            }
+
+            for (int f = 0; f < 4; f++)
+            {
+                buildings.Add(new Store(
+                    AirSpace.WIDTH / 8 + f * AirSpace.WIDTH / 4 + GlobalHelpers.alea.Next(0, 100) - 50,
+                    4*AirSpace.HEIGHT / 5 + GlobalHelpers.alea.Next(0, 100) - 50,
+                    new List<string>() { "Lu : 8h-18h", "Ma : 8h-18h", "Me : 8h-18h", "Je : 8h-18h", "Ve : 8h-18h", "Sa-Di : Fermé" }));
+            }
 
             // Démarrage
             Application.Run(new AirSpace(fleet, buildings));
