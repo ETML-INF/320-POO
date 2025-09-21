7b716725ecd8e3125dfa964859e0f8e136537e92
Xavier Carrel <Xavier.Carrel@eduvaud.ch>
2025-09-21 12:40:54 +0200

    feat(drones):  implement free flight

--- exos/Drones/Drones/Drones/Helpers/GlobalHelpers.cs
+++ exos/Drones/Drones/Drones/Helpers/GlobalHelpers.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace Drones
 {
-    internal static class GlobalHelpers
+    public static class GlobalHelpers
     {
         public static Random alea = new Random();
+        public static bool PointsAreClose(Point p1, Point p2)
+        {
+            return Math.Abs(p1.X - p2.X) < 4 && Math.Abs(p1.Y - p2.Y) < 4;
+        }
     }
+
 }
--- exos/Drones/Drones/Drones/Model/Drone.cs
+++ exos/Drones/Drones/Drones/Model/Drone.cs
@@ -3,7 +3,8 @@
     // Cette partie de la classe Drone définit ce qu'est un drone par un modèle numérique
     public partial class Drone
     {
-        public static readonly int FULLCHARGE = 1000;   // Charge maximale de la batterie
+        public static readonly int FULLCHARGE = 2000;   // Charge maximale de la batterie
+        public static readonly int STANDARD_SPEED = 4;  // Vitesse normale
         private int _charge;                            // La charge actuelle de la batterie
         private string _name;                           // Un nom
         private Point _position;
@@ -15,20 +16,51 @@
         public Drone(int x, int y, string name)
         {
             _position = new Point(x, y);
-            _speed = new Point(GlobalHelpers.alea.Next(0,8)-4, GlobalHelpers.alea.Next(0,8)-4);
+            _speed = new Point(GlobalHelpers.alea.Next(0, 8) - 4, GlobalHelpers.alea.Next(0, 8) - 4);
             _name = name;
             _charge = GlobalHelpers.alea.Next(FULLCHARGE); // La charge initiale de la batterie est choisie aléatoirement
+            _target = NewTarget();
         }
-        public string Name { get { return _name;} }
+        public string Name { get { return _name; } }
 
 
         // Cette méthode calcule le nouvel état dans lequel le drone se trouve après
         // que 'interval' millisecondes se sont écoulées
         public void Update(int interval)
         {
+            if (GlobalHelpers.PointsAreClose(_position, AirSpace.ChargingStation) && _charge < FULLCHARGE)
+            {
+                _charge += 10;
+                if (_charge >= FULLCHARGE)
+                {
+                    _charge = FULLCHARGE;
+
+                }
+                _speed.X = 0;
+                _speed.Y = 0;
+                return;
+            }
+            if (_charge-- < FULLCHARGE / 2)
+            {
+                _target = AirSpace.ChargingStation;
+            }
+            else
+            {
+                if (GlobalHelpers.PointsAreClose(_position, _target))
+                {
+                    _target = NewTarget();
+                }
+            }
+            _speed.X = Math.Abs(_position.X - _target.X) < 4 ? 0 : (_position.X > _target.X ? -STANDARD_SPEED : STANDARD_SPEED);
+            _speed.Y = Math.Abs(_position.Y - _target.Y) < 4 ? 0 : (_position.Y > _target.Y ? -STANDARD_SPEED : STANDARD_SPEED);
             _position.X += _speed.X;
             _position.Y += _speed.Y;
             _charge--;                                  // Il a dépensé de l'énergie
+        }
+
+        private Point NewTarget()
+        {
+            return new Point(GlobalHelpers.alea.Next(0, AirSpace.WIDTH), GlobalHelpers.alea.Next(0, AirSpace.HEIGHT));
         }
 
     }
